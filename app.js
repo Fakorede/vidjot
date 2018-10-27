d3014aaf225bb8af1018b8a2a2800717958af788
fakorede <abiolafakorede@gmail.com>
2018-10-27 14:20:13 +0100

    global variablee for user, passport and middleware for passport added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 // Initialize application
@@ -13,6 +14,9 @@ const app = express();
 // Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Map global promise - get rid of warnings
 mongoose.Promise = global.Promise;
@@ -47,6 +51,10 @@ app.use(session({
     saveUninitialized: true
   }))
 
+  // Passport middleware - always comes after express
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash middleware
 app.use(flash());
 
@@ -55,6 +63,7 @@ app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
